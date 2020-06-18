c14acdf236061b9a6cdf6a7adfca4c267b1d3cc9
Enno Hyttrek <enno.hyttrek@gmail.com>
2020-06-18 19:51:54 +0200

    Updated JS Menu Section

--- js-nav-menu/functions.js
+++ js-nav-menu/functions.js
@@ -2,10 +2,10 @@
 function createMenu() {
 var ul = document.createElement('ul');
 ul.innerHTML = `
-<li><a href="#">Menu Item 1</a></li>
-<li><a href="#">Menu Item 1</a></li>
-<li><a href="#">Menu Item 1</a></li>
-<li><a href="#">Menu Item 1</a></li>
+<li><a href="index.html">Home</a></li>
+<li><a href="page-01.html">Page One</a></li>
+<li><a href="page-02.html">Page Two</a></li>
+<li><a href="page-03.html">Page Three</a></li>
 `;
 document.getElementById('main-nav').appendChild(ul);
 }
