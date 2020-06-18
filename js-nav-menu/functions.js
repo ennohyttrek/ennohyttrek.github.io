26bd423fb68d128931c7b667ce5ac54d6a07fcb1
recode-lab <studio@recodelab.com>
2020-06-18 23:27:07 +0200

    Fallback

--- js-nav-menu/functions.js
+++ js-nav-menu/functions.js
@@ -2,7 +2,6 @@
 function createMenu() {
 var ul = document.createElement('ul');
 ul.innerHTML = `
-<li><a href="index.html">Home</a></li>
 <li><a href="page-01.html">Page One</a></li>
 <li><a href="page-02.html">Page Two</a></li>
 <li><a href="page-03.html">Page Three</a></li>
