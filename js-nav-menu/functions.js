0dd3f8b393e939ed8e48d6466c21e0a5380a1484
Enno Hyttrek <enno.hyttrek@gmail.com>
2020-06-20 10:30:44 +0200

    JS Menu added current item hilite

--- js-nav-menu/functions.js
+++ js-nav-menu/functions.js
@@ -1,10 +1,21 @@
-
 function createMenu() {
-var ul = document.createElement('ul');
-ul.innerHTML = `
-<li><a href="page-01.html">Page One</a></li>
-<li><a href="page-02.html">Page Two</a></li>
-<li><a href="page-03.html">Page Three</a></li>
+    var ul = document.createElement('ul');
+    ul.innerHTML = `
+<li id="nav-item-2"  class="nav-item" onclick="makeCurrent(this.id)"><a href="page-01.html">Page One</a></li>
+<li id="nav-item-3"  class="nav-item" onclick="makeCurrent(this.id)"><a href="page-02.html">Page Two</a></li>
+<li id="nav-item-4"  class="nav-item" onclick="makeCurrent(this.id)"><a href="page-03.html">Page Three</a></li>
 `;
-document.getElementById('main-nav').appendChild(ul);
+    document.getElementById('main-nav').appendChild(ul);
+    // document.getElementById(sessionStorage.getItem('myCurrent')).style.backgroundColor = 'red';
+    var y = sessionStorage.getItem('myCurrent');
+    document.getElementById(y).className = 'nav-item-current';
 }
+
+
+function makeCurrent(clicked_id) {
+  x = clicked_id;
+  sessionStorage.setItem('myCurrent', x);
+        document.getElementById(x).className = 'nav-item';
+} 
+
+
